a43032135b30c4e302ae141c0db17bb1f74b1a03
Muhammad Hidayat <49159167+codayid@users.noreply.github.com>
2019-06-02 16:52:15 +0700

    Create xmrcold.py

---
+++ xmrcold.py
@@ -0,0 +1,47 @@
+#! /usr/bin/python
+#Hapus Copyright?Lo Bukan Coder Bro
+
+#masuk tools
+
+yat = """ _____ ____   ____      _       _
+|_   _/ ___| / ___|_ __(_)_ __ | |_
+  | | \___ \| |   | '__| | '_ \| __|
+  | |  ___) | |___| |  | | |_) | |_
+  |_| |____/ \____|_|  |_| .__/ \__|
+                         |_|
+ __________________________________
+{ Author : CodayID || IG : hiidayt_}
+{ Script : xMr.Cold|| IG : aryaax_ }
+{  AnonCyberTeam - TEH Squad Cyber }
+{         Security Darknet         }
+{__________________________________}"""
+                         
+print yat
+ytitle = raw_input("<== Title ====> ")
+ynick = raw_input("<== Nick =====> ")
+yteam = raw_input("<== Team =====> ")
+ygrts = raw_input("<== Greetz ===> ")
+
+
+fo = open("xmrcold.html","w")
+ 
+codayid1 = """<html> <head> <title>"""
+codayid2 = ytitle
+codayid3 = """</title><style>html, body { color: #636b6f; font-family: 'Fredericka the Great', sans-serif; font-weight: 100; height: 100vh; margin: 0; } .full-height { height: 100vh; } .flex-center { align-items: center; display: flex; justify-content: center; } .position-ref { position: relative; } .content { text-align: center; } .title { font-size: 36px; padding: 20px; } </style> <center><style type="text/css"> * { margin:0; padding:0; } 	div { 	font-family: 'Merienda'; } html { background-color: #000000; font-family: Merienda; no-repeat center center fixed; -webkit-background-size: cover; -moz-background-size: cover; -o-background-size: cover; background-size: cover; } .text_d7net { font-weight:200px; font-size:20px; background:#000000; color:#3e6a06; text-shadow: -1px -1px 1px grey, -1px -1px 3px grey, 1px 1px 1px silver, 1px 1px 3px silver, 1px -1px 1px grey, -1px 1px 1px grey; } .sd7net { width: 100%; overflow:hidden; -webkit-animation: typing 5s steps(70, end); animation: animasi-ketik 5s steps(70, end); } @keyframes animasi-ketik{ from { width: 0; } } @-webkit-keyframes animasi-ketik{ from { width: 0; } } @import url(https://fonts.googleapis.com/css?family=Open+Sans:600); body { font-family: 'Open Sans', 'sans-serif'; color: #cecece; background: #000000; overflow: hidden; } .d7net-1, .d7net-2, .d7net-3, .d7net-4 { 	position: bottom; display: block; 	top: 2em; 	font-family: 'Merienda'; 	transparent; width: 60%; font-size: 24px; 	animation-duration: 15s; 	animation-timing-function: ease-in-out; 	animation-iteration-count: infinite; } .d7net-1{ 	animation-name: anim-1; } .d7net-2{ 	animation-name: anim-2; } .d7net-3{ animation-name: anim-3; } .d7net-4{ animation-name: anim-4; } @keyframes anim-1 { 	0%, 8.3% { left: -100%; opacity: 0; } 8.3%,25% { left: 25%; opacity: 1; } 33.33%, 100% { left: 110%; opacity: 0; } } @keyframes anim-2 { 	0%, 33.33% { left: -100%; opacity: 0; } 41.63%, 58.29% { left: 25%; opacity: 1; } 66.66%, 100% { left: 110%; opacity: 0; } } @keyframes anim-3 { 0%, 65.66% { left: -100%; opacity: 0; } 61.63%, 60.29% { left: 25%; opacity: 1; } 99.66%, 100% { left: 110%; opacity: 0; } } @keyframes anim-4 { 0%, 50.10% { left: -100%; opacity: 0; } 55.00%, 52.00% { left: 25%; opacity: 1; } 75.10%, 100% { left: 110%; opacity: 0; } } </style></center> </head> </head> <body align="center" oncontextmenu="return false" bgcolor="black"> <div class="stars"> <div class="twinkling"> <div class="flex-center position-ref full-height"> <div class="content"> <embed src="https://www.mediabantencyber.net/wilayah/Lil%20Pump%20ft.%206IX9INE%20-%20SHUT%20UP%20ft%20XXXTENTACION%20_%20S(MP3_70K).mp3" type="audio/mpeg" autostart="true" loop="true" height="0" width="0"> <div class="text"><font color="#FFFFFF"> <img src="https://i.ibb.co/ccctHL7/IMG-20190525-193436.jpg" height="300"> <br> <br> <font size="5"><b><i><code>"""
+codayid4 = ynick
+codayid5 = """</font> <br><br> <code>"""
+codayid6 = yteam
+cidayid7 = """<br><br> <center><p class="d7net-2">My Friends :</font> <br>"""
+codayid8 = ygrts
+codayid9 = """</font></center> </div> </div> </body> </html> <iframe width="0" height="0" src="http://lagu123.eu/play/alan-walker-k-391-emelie-hollow-lily~lagu123.eu~kTJbE3sfvlI.mp3" frameborder="0" allowfullscreen</iframe> """      
+
+fo.write(codayid1)
+fo.write(codayid2)
+fo.write(codayid3)
+fo.write(codayid4)
+fo.write(codayid5)
+fo.write(codayid6)
+fo.write(codayid8)
+fo.write(codayid9)
+fo.close()
+ 
